e655f97620ad978890e1d53e476ba39484c00ca4
ektoras <ekal2004@gmail.com>
2025-04-24 19:56:42 +0300

    updated hierarchy and added vigenere encyption

--- cipher_analysis/vigenere_cipher/vigenere_encryption.py
+++ cipher_analysis/vigenere_cipher/vigenere_encryption.py
@@ -1,25 +1,36 @@
-def encrypt(text, shift):
+def vigenere_encrypt(text, key):
     result = ""
+    key = key.upper()
+    key_index = 0
+
     for char in text:
         if char.isalpha():
-            base = ord('a') if char.islower() else ord('A')
+            base = ord('A') if char.isupper() else ord('a')
+            shift = ord(key[key_index % len(key)]) - ord('A')
             shifted_char = chr((ord(char) - base + shift) % 26 + base)
             result += shifted_char
+            key_index += 1
+
         elif char.isdigit():
+            shift = ord(key[key_index % len(key)]) - ord('A')
             shifted_num = str((int(char) + shift) % 10)
             result += shifted_num
+            key_index += 1
+
         else:
             result += char
+
     return result
 
+
 file = input("Specify file path or name: ")
-shift = 3
+
+key = input("Specify the key")
 
 with open(file, 'r+') as f:
     content = f.read()
-    enc_content = encrypt(content,shift)
+    enc_content = vigenere_encrypt(content,key)
     f.seek(0)
     f.write(enc_content)
     f.truncate()
 
-print(enc_content)
